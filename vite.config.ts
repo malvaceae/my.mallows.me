f337fc75c7ca178aafb6b0b0d9945f31fa688c5b
malvaceae <malvaceae@hotmail.com>
2026-01-24 03:26:16 +0900

    change output file names

--- vite.config.ts
+++ vite.config.ts
@@ -30,4 +30,36 @@ export default defineConfig({
       runtime: 'nodejs22.x',
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames({ source }) {
+          if (typeof source === 'string') {
+            return '[ext]/[hash][extname]';
+          }
+
+          // WOFF/WOFF2
+          if (
+            source[0] === 0x77 && source[1] === 0x4F && source[2] === 0x46 && source[3] === 0x46 ||
+            source[0] === 0x77 && source[1] === 0x4F && source[2] === 0x46 && source[3] === 0x32
+          ) {
+            return 'fonts/[hash][extname]';
+          }
+
+          // GIF/JPEG/PNG
+          if (
+            source[0] === 0x47 && source[1] === 0x49 && source[2] === 0x46 && source[3] === 0x38 ||
+            source[0] === 0xFF && source[1] === 0xD8 ||
+            source[0] === 0x89 && source[1] === 0x50 && source[2] === 0x4E && source[3] === 0x47
+          ) {
+            return 'img/[hash][extname]';
+          }
+
+          return '[ext]/[hash][extname]';
+        },
+        chunkFileNames: 'js/[hash].js',
+        entryFileNames: 'js/[hash].js',
+      },
+    },
+  },
 });
